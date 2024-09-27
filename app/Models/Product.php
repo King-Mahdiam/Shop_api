a34a3d659b1568c79953b9369cd2e6e7c20a2c63
King-Mahdiam <mahdidarrudi0930.ha@gmail.com>
2024-09-27 22:22:05 +0330

    Edit All The Connections For The Product Model In Project

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -29,5 +29,10 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
-    
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
 }
