f073b2ecafc1b2df2f2918e29514687d2d8092d5
King-Mahdiam <mahdidarrudi0930.ha@gmail.com>
2024-09-27 22:25:38 +0330

    Add All The Connections For The Province Model In Project

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -12,4 +12,13 @@ class Province extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function cities()
+    {
+        return $this->hasMany(City::class);
+    }
+
+    public function users() {
+        return $this->hasMany(User::class);
+    }
 }
